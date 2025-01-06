f716a841464ba4aa376b4bf29090a06d08c7834b
slimmpylk <slimmpylk@proton.me>
2025-01-06 12:42:07 +0200

    Finally everything seems to be working.
    Website updates automatically whenever main branch is modified

--- src/components/homePage/HeroSection.tsx
+++ src/components/homePage/HeroSection.tsx
@@ -6,22 +6,22 @@ export default function HeroSection() {
         <header className="bg-black text-white min-h-screen flex flex-col items-center justify-center py-16">
             {/* Title in the Middle */}
             <h1 className="text-5xl sm:text-6xl font-extrabold bg-gradient-to-r from-orange-500 via-yellow-500 to-orange-500 bg-clip-text text-transparent mb-12 text-center">
-                CipherPairv TEST1465
+                CipherPair
             </h1>
 
             {/* Main Section */}
             <div className="flex flex-col md:flex-row items-center justify-center gap-16 w-full px-4 sm:px-16">
-                {/* Left Section (Your Info) */}
+                {/* Left Section (Samuli Info) */}
                 <div className="flex flex-col items-center text-center">
                     <Image
                         src="/photos/Samuli.jpg"
                         alt="Samuli's Profile Picture"
-                        className="rounded-full border-4 border-orange-500 shadow-lg"
+                        className="rounded-full border-4 border-orange-500 shadow-[0px_0px_12px_1px_rgba(255,215,0,1)] "
                         width={150}
                         height={150}
                     />
                     <h2 className="text-2xl font-bold mt-4">Samuli Pylkkönen (CEO)</h2>
-                    <p className="text-gray-300 mt-2">Junior Developer & Cybersecurity Enthusiast</p>
+                    <p className="text-gray-300 mt-2">Junior developer & Cybersecurity Enthusiast</p>
                     <Link href="/samuliPortfolio">
                         <button className="mt-4 px-6 py-2 bg-orange-500 text-white font-bold rounded hover:bg-orange-600">
                             Samuli&apos;s Portfolio
@@ -32,7 +32,7 @@ export default function HeroSection() {
                 {/* Right Section (Friend's Info) */}
                 <div className="flex flex-col items-center text-center">
                     <Image
-                        src="/photos/Friend.jpg" // Replace this with your friend's picture path
+                        src="/photos/Friend.jpg" // Replace this with friend's picture path
                         alt="Friend's Profile Picture"
                         className="rounded-full border-4 border-orange-500 shadow-lg"
                         width={150}
--- src/components/samuliPortfolio/AboutSection.tsx
+++ src/components/samuliPortfolio/AboutSection.tsx
@@ -5,12 +5,11 @@ export default function AboutSection() {
                 <h2 className="title text-2xl sm:text-3xl font-bold mb-8">About Me</h2>
                 <div className="max-w-3xl mx-auto">
                     <p className="text-sm sm:text-base lg:text-lg leading-relaxed mb-10">
-                        Hello! I&#39;m Samuli, an ultra-runner, junior  programmer, and aspiring cybersecurity specialist from Finland.
+                        Hello! I&#39;m Samuli, an ultra-runner, junior  developer, and aspiring cybersecurity specialist from Finland.
                     </p>
                     <p className="text-sm sm:text-base lg:text-lg leading-relaxed mb-10">
                         In my free time, I love pushing my limits through running—it’s the perfect way to balance out all the sitting and screen time.
-                        At the same time, I’m constantly working on improving my IT skills to become a true professional. My programming journey started
-                        with my academic studies and has grown through personal projects that challenge and inspire me.
+                        At the same time, I’m constantly working on improving my IT skills to become a true professional. My passion for programming and technology began in childhood, but it truly evolved into a serious and professional pursuit during my academic journey, where personal projects have continually challenged and inspired me to grow. Like this website.
                     </p>
                     <p className="text-sm sm:text-base lg:text-lg leading-relaxed">
                         What drives me even further is my passion for Cybersecurity. I&#39;m currently immersing myself in this dynamic field, eager to master
